4a4b7c5ec8a2f090cbff0888a8f19ef297257da3
Simon Parker <simon.parker.bc@gmail.com>
2016-08-12 07:48:43 -0400

    Add many go blocks example and modify dependencies

--- src/clojure_lunch_async/core.clj
+++ src/clojure_lunch_async/core.clj
@@ -1,1 +1,18 @@
-(ns clojure-lunch-async.core)
+(ns clojure-lunch-async.core
+  (:require [clojure.core.async :as async :refer [chan timeout
+                                                  buffer sliding-buffer dropping-buffer
+                                                  <! <!! >! >!!
+                                                  go go-loop thread
+                                                  alt! alt!! alts! alts!!
+                                                  poll! offer!
+                                                  onto-chan to-chan
+                                                  close!
+                                                  mix admix unmix]]))
+
+(defn merge-go-blocks [n]
+  (let [chans (map #(go %) (range 1 n))
+        merged-chan (async/merge chans)]
+    (alts!! [(async/reduce + 0 merged-chan)
+             (timeout 5000)])))
+
+(merge-go-blocks 1000)
